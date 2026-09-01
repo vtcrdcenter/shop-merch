1601fb96ff0d1331b639761121ac81e36f06a51a
vtcrdcenter <vtcrdcenter@gmail.com>
2026-09-01 15:13:23 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages =
+  process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  trailingSlash: true,
+
+  basePath: isGitHubPages
+    ? "/shop-merch"
+    : "",
+
+  assetPrefix: isGitHubPages
+    ? "/shop-merch/"
+    : "",
 };
 
 export default nextConfig;
